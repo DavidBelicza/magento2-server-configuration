a411387aba146e49c79b2b5df39704bd6b2a983d
DoveID <87.bdavid@gmail.com>
2016-05-28 10:36:41 +0200

    PHP7

---
+++ install/php7-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo apt-get install \
+    php-fpm \
+    php-mysql \
+    php-mcrypt \
+    php-curl \
+    php-cli \
+    php-gd \
+    php7.0-xsl \
+    php-json \
+    php-intl \
+    php-pear \
+    php-dev \
+    php-common \
+    php-soap \
+    --yes
