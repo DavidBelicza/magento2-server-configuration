23b71ee83bcf591056e1e52b45eab490b7b1833c
Darius de Witt <darius.dewitt@lybe.se>
2017-08-01 17:22:12 +0200

    force_apache2_mpm_worker

--- install/apache.sh
+++ install/apache.sh
@@ -2,6 +2,13 @@
 
 apt-get install \
     apache2-mpm-worker \
+    libapache2-mod-fastcgi \
     --yes
+
+a2enmod \
+    fastcgi \
+    proxy_fcgi \
+    alias \
+    actions
 
 service apache2 restart
